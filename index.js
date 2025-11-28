eca2d091408ee17e5772a09162009c4f46b8bf10
SamuelPeAg <spa0004@alu.medac.es>
2025-11-28 23:58:23 +0100

    modal hecho

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ document.querySelectorAll('[data-sonido="true"]').forEach(el => {
         }, 300); // 120 ms
     });
 });
+
+const pagarButton = document.querySelector('#modal');
+
+// Seleccionamos el botón "Cerrar"
+const closeModalButton = document.querySelector('#closeModal');
+
+closeModalButton.addEventListener('click', function() {
+    window.location.href = './carrito.html';
+
+    setTimeout(function() {
+        location.reload();
+    }, 1000); // Se espera 1 segundo antes de recargar
+});
+
+
